1abeea177b74329f79dcc3328040936cd5dc8e48
Kudratov1412 ishxona <furqatkudratov545@gmail.com>
2026-07-18 15:37:28 +0500

    Course Detail ga tayyorgarlik

--- 86-dars_e-learning/src/pages/Courses.js
+++ 86-dars_e-learning/src/pages/Courses.js
@@ -1,11 +1,57 @@
 import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+// Images
+import pic1 from "./../img/1-pic.jpg";
+import pic2 from "./../img/2-pic.jpg";
+import pic3 from "./../img/3-pic.jpg";
 
 const Courses = () => {
   return (
-    <div>
-      <h1>Courses</h1>
-    </div>
+    <StyledCourses>
+      <Course>
+        <h2>Interactiv React va Redux</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={pic1} alt="react-redux" />
+        </Link>
+      </Course>
+      <Course>
+        <h2>Tez kunda 😮</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={pic2} alt="tez kunda" />
+        </Link>
+      </Course>
+      <Course>
+        <h2>Interactiv JavaScript</h2>
+        <div className="line"></div>
+        <Link>
+          <img src={pic3} alt="javascript" />
+        </Link>
+      </Course>
+    </StyledCourses>
   );
 };
+
+const StyledCourses = styled.div`
+  min-height: 10vh;
+  padding: 5rem 10rem;
+  img {
+    width: 800px;
+  }
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Course = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    height: 0.5rem;
+    background: #ccc;
+    margin-bottom: 3rem;
+  }
+`;
 
 export default Courses;
---
+++ 86-dars_e-learning/src/pages/courseState.js
@@ -0,0 +1,81 @@
+// Images
+import react1 from "./../img/react.jpg";
+import react2 from "./../img/react2.jpg";
+import js1 from "./../img/js.jpg";
+import js2 from "./../img/js2.jpg";
+import soon1 from "./../img/soon.jpg";
+import soon2 from "./../img/soon2.jpg";
+
+export const CourseState = () => {
+  return [
+    {
+      title: "Interactiv React va Redux",
+      mainImg: react1,
+      secondaryImg: react2,
+      url: "/courses/react",
+      awards: [
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+      ],
+    },
+    {
+      title: "Tez kunda",
+      mainImg: soon1,
+      secondaryImg: soon2,
+      url: "/courses/soon",
+      awards: [
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+      ],
+    },
+    {
+      title: "Interactiv JavaScript",
+      mainImg: js1,
+      secondaryImg: js2,
+      url: "/courses/javascript",
+      awards: [
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+        {
+          title: "Lorem ipsum sit",
+          description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque explicabo iure omnis eligendi quibusdam maxime.",
+        },
+      ],
+    },
+  ];
+};
