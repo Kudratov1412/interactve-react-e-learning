42a6f3557882f8df944d4be895ba1d11b96eb36d
Kudratov1412 ishxona <furqatkudratov545@gmail.com>
2026-07-18 14:41:53 +0500

    React Router Dom  and Link component adding

--- 86-dars_e-learning/src/index.js
+++ 86-dars_e-learning/src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// React Router
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
 reportWebVitals();
