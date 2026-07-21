97434d51271a53df0fdb40182d685f00657cb647
Kudratov1412 <furqatkudratov545@gmail.com>
2026-07-22 00:10:17 +0500

    Framer-motion 1-part

--- 86-dars_e-learning/src/components/AboutSection.js
+++ 86-dars_e-learning/src/components/AboutSection.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useTransition } from "react";
 // import images
 import homeIntro from "./../img/home_intro.jpg";
 // import styled
 import styled from "styled-components";
 import { About, Description, Image, Hide } from "../styles";
+// framer motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 1.2 } },
+  };
+
   return (
     <About>
       <Description>
         <div className="title">
           <Hide>
-            <h2>Be educated so that</h2>
+            <motion.h2 variants={titleAnim} initial="hidden" animate="show">
+              Be educated so that
+            </motion.h2>
           </Hide>
           <Hide>
             <h2>
